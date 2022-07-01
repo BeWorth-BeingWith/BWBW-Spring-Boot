76e78e9fe81e718436ee6be0af5df4c7ebd508d0
BYEONGJUN KANG <lucid9dawn@gmail.com>
2022-07-01 16:24:17 +0900

    Generate bwbw Project for clone

---
+++ src/test/java/com/example/bwbw/BwbwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bwbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwbwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
