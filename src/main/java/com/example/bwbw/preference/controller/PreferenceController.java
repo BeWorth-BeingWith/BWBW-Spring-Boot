bd14280bf4dc68f200d3360066f4bcab556f3799
Yoo Haechan <gocksdl7979@gmail.com>
2022-07-07 16:21:33 +0900

    [수정] AccessToken 값들 제거

--- src/main/java/com/example/bwbw/preference/controller/PreferenceController.java
+++ src/main/java/com/example/bwbw/preference/controller/PreferenceController.java
@@ -16,15 +16,9 @@ import java.util.List;
 public class PreferenceController {
 
     @ApiOperation(value="review 보기", notes = "지금 까지 후기 모두 가져오기")
-    @ApiImplicitParams(
-            {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class)
-            }
-    )
     @ApiResponse(code=200, message = "성공 / content가 비어있으면 Empty")
     @GetMapping("/my-review")
-    public ResponseEntity<List<ReviewResponseDto>> reviewRead(@RequestBody String accessToken, @RequestBody LocalDateTime expiredTime){
+    public ResponseEntity<List<ReviewResponseDto>> reviewRead(){
 
         //TODO: Database에서 리뷰 내용을 가져오는 Serivce 로직을 실행, 없으면 데이터는 null status = 200 보냄
 
@@ -32,15 +26,9 @@ public class PreferenceController {
     }
 
     @ApiOperation(value="review 작성 페이지만 이동", notes = "지금 까지 리뷰 작성이 안된 페이지들만 가져오기")
-    @ApiImplicitParams(
-            {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class)
-            }
-    )
     @ApiResponse(code=200, message = "성공 / content가 비어있으면 모두 리뷰를 남긴 것")
     @GetMapping("/write-review")
-    public ResponseEntity<List<ReviewResponseDto>> reviewWrite(@RequestBody String accessToken, @RequestBody LocalDateTime expiredTime){
+    public ResponseEntity<List<ReviewResponseDto>> reviewWrite(){
         //TODO: 리뷰 작성 안된 인원들만 추려서 데이터 전송
 
         return null;
@@ -50,9 +38,7 @@ public class PreferenceController {
     @ApiOperation(value="review 작성 페이지", notes = "지금 까지 리뷰 작성이 안된 사람들만 가져오기")
     @ApiImplicitParams(
             {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class),
-                    @ApiImplicitParam(name = "roomId", value = "이용 했던 방 고유값", dataTypeClass = LocalDateTime.class)
+                    @ApiImplicitParam(name = "roomId", value = "이용 했던 방 고유값")
             }
     )
     @ApiResponses({
@@ -61,7 +47,7 @@ public class PreferenceController {
         }
     )
     @GetMapping("write-review/write")
-    public ResponseEntity<List<String>> reviewWritePage(@RequestBody String accessToken, @RequestBody LocalDateTime expiredTime, @RequestBody Long roomId){
+    public ResponseEntity<List<String>> reviewWritePage(@RequestParam(name = "roomId") Long roomId){
         //TODO: 리뷰 작성하지 않은 이름들만 넘겨주는 로직 구현
         return null;
     }
@@ -69,8 +55,6 @@ public class PreferenceController {
     @ApiOperation(value = "사람별 리뷰 작성", notes = "선택한 사람 이름, 리뷰 내용, 방 번호, 액세스토큰들을 받고 리뷰작성 성공 여부를 넘깁니다.")
     @ApiImplicitParams(
             {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class),
                     @ApiImplicitParam(name = "name", value = "리뷰 작성 대상자 이름", dataTypeClass = String.class),
                     @ApiImplicitParam(name = "review", value = "리뷰 내용", dataTypeClass = String.class),
                     @ApiImplicitParam(name = "roomId", value = "방 번호", dataTypeClass = Long.class)
@@ -88,15 +72,9 @@ public class PreferenceController {
     }
 
     @ApiOperation(value="bage 확인", notes = "지금 까지 모은 badge들 확인")
-    @ApiImplicitParams(
-            {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class)
-            }
-    )
     @ApiResponse(code=200, message = "성공 / content가 비어있으면 모두 리뷰를 남긴 것")
     @GetMapping("/badge")
-    public ResponseEntity<BadgeResponseDto> badgeRead(@RequestBody String accessToken,@RequestBody LocalDateTime expiredTime){
+    public ResponseEntity<BadgeResponseDto> badgeRead(){
         //TODO: 정보 읽어오는 로직 만들기
         return null;
     }
@@ -104,8 +82,6 @@ public class PreferenceController {
     @ApiOperation(value="회원 탈퇴", notes = "회원 탈퇴를 진행")
     @ApiImplicitParams(
             {
-                    @ApiImplicitParam(name = "accessToken", value = "유저 엑세스 토큰", dataTypeClass = String.class),
-                    @ApiImplicitParam(name = "expiredTime", value = "토큰 만료 시간", dataTypeClass = LocalDateTime.class),
                     @ApiImplicitParam(name = "password", value = "패스 워드", dataTypeClass = String.class)
             }
     )
@@ -114,7 +90,7 @@ public class PreferenceController {
             @ApiResponse(code=401, message = "일치하지 않음")
     })
     @DeleteMapping("/delete/user")
-    public ResponseEntity<Void> deleteUser(@RequestBody String accessToken, @RequestBody LocalDateTime expiredTime, @RequestBody String password){
+    public ResponseEntity<Void> deleteUser(@RequestParam(name = "password") String password){
 
         //TODO: 회원 탈퇴 로직 생성
 
