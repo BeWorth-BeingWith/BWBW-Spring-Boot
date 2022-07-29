fc72baeb58a30b53e4a60a4b567a252814330175
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2022-07-29 16:31:38 +0900

    Feature #6 preference (#7)
    * [추가] 리뷰 정보 불러오기 작성
    
    * [추가] 방 제목 항목 추가
    
    * [추가] 리뷰 작성 페이지로 이동 추가
    
    * [추가] 리뷰 작성 세부페이지 추가
    
    * [추가] roomId 타입 변경 및 리뷰 생성 추가
    
    * [수정] 지우지 않은 부분 삭제
    
    * [추가] 만료시간 추가
    
    * [추가] 만료시간 타입 변경
    
    * [수정] 만료시간 이름 수정
    
    * [추가] 모은 뱃지 확인
    
    * [추가] dataTypeClass 추가
    
    * [추가] RequestBody 어노테이션 추가
    
    * [추가] expiredTime 누락 추가
    
    * [수정] AccessToken 값들 제거

---
+++ src/main/java/com/example/bwbw/preference/dto/ReviewResponseDto.java
@@ -0,0 +1,30 @@
+package com.example.bwbw.preference.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value = "리뷰 정보", description = "방 제목, 매칭한 인원 수, 식당 이름, 매칭 시간, 리뷰등이 적혀 있습니다.")
+public class ReviewResponseDto {
+
+    @ApiModelProperty(value = "방 제목", example = "해찬식당 뿌시러갈 사람들!!!")
+    private String title;
+
+    @ApiModelProperty(value = "매칭 시간")
+    private LocalDateTime matchingTime;
+
+    @ApiModelProperty(value = "식당 이름", example = "해찬식당")
+    private String restaurantName;
+
+    @ApiModelProperty(value = "매칭한 유저", example = "4")
+    private Integer matchingUser;
+
+    @ApiModelProperty(value = "리뷰 내용", example = "식당은 짜고 맛이 없었습니다\n 같이온 맴버들이랑 맛있게 먹었습니다!!")
+    private String review;
+}
