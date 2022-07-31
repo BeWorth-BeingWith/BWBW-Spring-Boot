0491a4581caa250e4ffaa81b65108047847eeb5c
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2022-07-31 18:38:00 +0900

    Feature #11 user/room entity (#12)
    * [추가] jpa 설정 추가
    
    * [추가] USER_INFO, ROOM 테이블 entity 추가
    
    * [추가] application.properties 추가
    
    * [추가] db 정보 추가

---
+++ src/main/java/com/example/bwbw/entity/UserInfoEntity.java
@@ -0,0 +1,61 @@
+package com.example.bwbw.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "USER_INFO")
+public class UserInfoEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    @NotNull
+    private String nickname;
+
+    @Column(length = 30)
+    @NotNull
+    private String password;
+
+    @Column(length = 100)
+    @NotNull
+    private String email;
+
+    @Column
+    @NotNull
+    private Integer gender;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    @Column
+    private String major;
+
+    @Column(name = "modified_major_at")
+    private LocalDateTime modifiedMajorAt;
+
+    @OneToMany(mappedBy = "user")
+    List<RoomEntity> rooms = new ArrayList<>();
+
+}
