a8b139c78631509311366b62c512a3212c100954
leech1211 <leech1211@nate.com>
2022-08-05 21:07:08 +0900

    [수정] 위도경도 변수명 수정

--- src/main/java/com/example/bwbw/mainscreen/dto/HomeRestaurant.java
+++ src/main/java/com/example/bwbw/mainscreen/dto/HomeRestaurant.java
@@ -16,10 +16,10 @@ import java.util.List;
 @NoArgsConstructor
 public class HomeRestaurant {
     @ApiModelProperty(value = "위도", example = "37.620170")
-    private double x;
+    private double latitude;
 
     @ApiModelProperty(value = "경도", example = "127.061323")
-    private double y;    //좌표
+    private double longitude;    //좌표
 
     @ApiModelProperty(value = "식당이름", example = "작은집")
     private String name;    //식당이름
